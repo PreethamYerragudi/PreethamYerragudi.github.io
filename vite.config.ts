0267727a19fe9a9d2e5b79beb7a880bc78be7da9
PreethamYerragudi <preetham.yerragudi@gmail.com>
2025-01-31 15:59:34 -0500

    changed build settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // Change the `base` URL to match your repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/PreethamYerragudi.github.io/'
-})
+  base: '/PreethamYerragudi.github.io/', // Ensure this matches your repository name exactly
+  build: {
+    outDir: 'dist', // Ensure the build outputs to the 'dist' directory
+    emptyOutDir: true, // Ensure the 'dist' directory is emptied before each build
+  },
+});
