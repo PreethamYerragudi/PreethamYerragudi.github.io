b5651c75260ca05e3162226d44c1f1dcff12cd8e
PreethamYerragudi <preetham.yerragudi@gmail.com>
2025-01-31 14:50:40 -0500

    Configured Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
+// Change the `base` URL to match your repository name
 export default defineConfig({
   plugins: [react()],
+  base: '/PreethamYerragudi.github.io/' // 🔹 Update with your GitHub repo name
 })
